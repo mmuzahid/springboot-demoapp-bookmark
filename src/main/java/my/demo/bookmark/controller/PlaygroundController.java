81396e71b5981313075508879b67153f33e1412c
mmuzahid <muzahid.engr@gmail.com>
2020-06-05 23:29:52 +0600

    Added PlaygroundController: to Practice features of Spring / Spring-Boot

---
+++ src/main/java/my/demo/bookmark/controller/PlaygroundController.java
@@ -0,0 +1,25 @@
+package my.demo.bookmark.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import my.demo.bookmark.service.RequestScopeBean;
+import my.demo.bookmark.service.SessionScopeBean;
+
+@Controller
+@RequestMapping("/playground")
+public class PlaygroundController {
+	
+	@GetMapping("/")
+	public String home(Model model) {
+
+		return "playgroundHome";
+	}
+
+}
