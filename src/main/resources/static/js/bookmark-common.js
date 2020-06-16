8f9ac1c88228b38289df1ad73016d84088464b3a
mmuzahid <muzahid.engr@gmail.com>
2020-06-16 10:33:18 +0600

    Added Tag View Templates and JS functions

--- src/main/resources/static/js/bookmark-common.js
+++ src/main/resources/static/js/bookmark-common.js
@@ -14,10 +14,35 @@ function confirmBookmarkDelete(bookmarkId, afterDeleteAction) {
 	}
 }
 
+
+function confirmTagDelete(tagId, afterDeleteAction) {
+	if(confirm("Record will be deleted. Are you sure?")) {
+		jQuery.ajax({
+			url: '/tag/' + tagId,
+			type: 'DELETE',
+			success: function(result) {
+				alert('Record deleted Successfully!');
+				afterDeleteAction(); 
+			},
+			error: function(result) {
+				alert('Something wrong , server Failed to delete this record!');
+			}
+		});
+	}
+}
+
 function pageRefresh(){
 	bookmarkTable.setPage(1);// TODO: stay at current page or go to previous page if no data found in current page
 }
 
+function refreshPageTagTable(){
+	tagTable.setPage(1);// TODO: stay at current page or go to previous page if no data found in current page
+}
+
 function goHome(){
 	window.location.href = '/bookmark/';
+}
+
+function goTagHome(){
+	window.location.href = '/tag/';
 }
