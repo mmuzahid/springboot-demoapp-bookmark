776424c0fd060b1d89905c425e63cff99a5bea33
mmuzahid <muzahid.engr@gmail.com>
2020-06-19 01:17:47 +0600

    Refactor - Rename property 'active' to 'inactive'

--- src/main/java/my/demo/bookmark/entity/Tag.java
+++ src/main/java/my/demo/bookmark/entity/Tag.java
@@ -16,5 +16,5 @@ public class Tag {
 	private Long id;
 	@Size(min = 2, max = 20, message = "length of tag must be between 2 and 20")
 	private String name;
-	private boolean active;
+	private boolean inactive;
 }
