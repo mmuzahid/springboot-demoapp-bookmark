9a797fa0a3c1b4518cbb0227ecc1e07b6b526ca8
mmuzahid <muzahid.engr@gmail.com>
2020-07-05 01:39:40 +0600

    Added logger to GlobalDefaultExceptionHandler

--- src/main/java/my/demo/bookmark/exception/handler/GlobalDefaultExceptionHandler.java
+++ src/main/java/my/demo/bookmark/exception/handler/GlobalDefaultExceptionHandler.java
@@ -2,6 +2,8 @@ package my.demo.bookmark.exception.handler;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,25 +13,27 @@ import org.springframework.web.servlet.ModelAndView;
 
 @ControllerAdvice
 class GlobalDefaultExceptionHandler {
-  public static final String DEFAULT_ERROR_VIEW = "error";
+	private static Logger logger = LoggerFactory.getLogger(GlobalDefaultExceptionHandler.class); 
 
-  @ExceptionHandler(value = Exception.class)
-  public ModelAndView
-  defaultErrorHandler(HttpServletRequest req, Exception e) throws Exception {
-    // If the exception is annotated with @ResponseStatus rethrow it and let
-    // the framework handle it - like the OrderNotFoundException example
-    // at the start of this post.
-    // AnnotationUtils is a Spring Framework utility class.
-    if (AnnotationUtils.findAnnotation
-                (e.getClass(), ResponseStatus.class) != null)
-      throw e;
+	public static final String DEFAULT_ERROR_VIEW = "error";
 
-    // Otherwise setup and send the user to a default error-view.
-    ModelAndView mav = new ModelAndView();
-    mav.addObject("exception", e);
-    mav.addObject("url", req.getRequestURL());
-    mav.setViewName(DEFAULT_ERROR_VIEW);
-    mav.setStatus(HttpStatus.BAD_REQUEST);
-    return mav;
-  }
+	@ExceptionHandler(value = Exception.class)
+	public ModelAndView defaultErrorHandler(HttpServletRequest req, Exception ex) throws Exception {
+		logger.info("global exception handler.  ex : {}", ex.getStackTrace());
+		
+		// If the exception is annotated with @ResponseStatus rethrow it and let
+		// the framework handle it - like the OrderNotFoundException example
+		// at the start of this post.
+		// AnnotationUtils is a Spring Framework utility class.
+		if (AnnotationUtils.findAnnotation(ex.getClass(), ResponseStatus.class) != null)
+			throw ex;
+
+		// Otherwise setup and send the user to a default error-view.
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("exception", ex);
+		mav.addObject("url", req.getRequestURL());
+		mav.setViewName(DEFAULT_ERROR_VIEW);
+		mav.setStatus(HttpStatus.BAD_REQUEST);
+		return mav;
+	}
 }
