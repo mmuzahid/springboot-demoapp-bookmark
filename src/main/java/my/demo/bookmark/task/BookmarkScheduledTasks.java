296164bd8ed7c28b74ad203d365cb41bf685692b
mmuzahid <muzahid.engr@gmail.com>
2020-07-09 11:54:05 +0600

    Task Scheduler - Added Transactional method to Delete Old Bookmarks

--- src/main/java/my/demo/bookmark/task/BookmarkScheduledTasks.java
+++ src/main/java/my/demo/bookmark/task/BookmarkScheduledTasks.java
@@ -1,6 +1,9 @@
 package my.demo.bookmark.task;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import javax.transaction.Transactional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,16 +13,21 @@ import org.springframework.stereotype.Component;
 
 import my.demo.bookmark.service.BookmarkService;
 @Component
-public class ScheduledBookmarTasks {
+public class BookmarkScheduledTasks {
 
-	private static final Logger log = LoggerFactory.getLogger(ScheduledBookmarTasks.class);
+	private static final Logger log = LoggerFactory.getLogger(BookmarkScheduledTasks.class);
+
+	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
 	@Autowired
 	private BookmarkService bookmarkService;
 	
 	@Scheduled(fixedRate = 5000)
-	public void archiveBookmarks() {
-		log.info("TODO: Archive Old Bookmarks: The time is now {}", new Date());
+	@Transactional
+	public void deleteOldBookmarks() {
+		log.info("TODO: Archive Old Bookmarks: The time is now {}", dateFormat.format(new Date()));
+		Date yesterday = new Date(System.currentTimeMillis() - 24*60*60*1000);
+		bookmarkService.deleteBookmarksDateLessThan(yesterday);
 	}
 	
 }
