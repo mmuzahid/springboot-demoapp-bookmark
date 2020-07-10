e751625b432b719e44a0b8e57974cd32f393fd48
mmuzahid <muzahid.engr@gmail.com>
2020-07-10 10:13:22 +0600

    Added serialVersionUID and default Constructor to BookmarkException

--- src/main/java/my/demo/bookmark/exception/BookmarkException.java
+++ src/main/java/my/demo/bookmark/exception/BookmarkException.java
@@ -2,6 +2,16 @@ package my.demo.bookmark.exception;
 
 public class BookmarkException extends RuntimeException {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
+	public BookmarkException() {
+		super();
+	}
+	
 	public BookmarkException(String message) {
 		super(message);
 	}
