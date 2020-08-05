28bb1dc493622134ca6a8375da89aeba4c82f3bf
mmuzahid <muzahid.engr@gmail.com>
2020-08-06 01:36:06 +0600

    Spring Security - User from Database : Security Configuration for DbUser
    1. Added PasswordEncoder Bean - BCryptPasswordEncoder
    2. Added databaseUserAuthenticationProvider Bean

--- src/main/java/my/demo/bookmark/configuration/WebSecurityConfig.java
+++ src/main/java/my/demo/bookmark/configuration/WebSecurityConfig.java
@@ -1,16 +1,36 @@
 package my.demo.bookmark.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	
+	@Autowired
+    private PasswordEncoder passwordEncoder;
+	
+	@Autowired
+	@Qualifier("databaseUserDetailsService")
+	private UserDetailsService databaseUserDetailsService;
+	
+	@Autowired
+	@Qualifier("databaseUserAuthenticationProvider")
+	private AuthenticationProvider databaseUserAuthenticationProvider;
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http        
@@ -30,11 +50,31 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-        auth.inMemoryAuthentication()
-                .withUser("user").password("{noop}password").roles("USER")
-                .and()
-                .withUser("admin").password("{noop}password").roles("USER", "ADMIN");
-
+	
+		auth.authenticationProvider(databaseUserAuthenticationProvider);
+		auth.inMemoryAuthentication()
+		  .withUser("user").password(passwordEncoder.encode("password")).roles("USER")
+		  .and()
+		  .withUser("admin").password(passwordEncoder.encode("password")).roles("USER",
+		  "ADMIN");
+		 
     }
+	
+	
+	@Bean(name="databaseUserAuthenticationProvider")
+	public DaoAuthenticationProvider daoAuthenticationProvider() {
+		DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider(); 
+		daoAuthenticationProvider.setPasswordEncoder(passwordEncoder);
+		daoAuthenticationProvider.setUserDetailsService(databaseUserDetailsService);
+		
+		return daoAuthenticationProvider;
+	}
+	
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	 
+	
 }
